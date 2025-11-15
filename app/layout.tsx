0951b14365796232f18fe7ae0fc8e0db6edfcdcb
Diego Sarmiento <dlsarmiento@gmail.com>
2025-11-15 17:50:14 -0500

    Added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,21 @@ import '@/styles/globals.css'
 import Nav from '@/components/Nav'
 import SiteFooter from '@/components/SiteFooter'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 
 const inter = Inter({
   subsets: ['latin'],
   display: 'swap',
 })
 
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+
+const GOOGLE_ANALYTICS_SCRIPT = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${GA_ID}');
+`
 
 export const metadata: Metadata = {
   title: {
@@ -27,6 +36,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <body
         className={`${inter.className} min-h-dvh flex flex-col bg-[rgb(var(--background))] text-[rgb(var(--foreground))]`}
       >
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        />
+        <Script
+          id="ga-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{ __html: GOOGLE_ANALYTICS_SCRIPT }}
+        />
         <Nav />
         <main className="flex-1">{children}</main>
         <SiteFooter />
